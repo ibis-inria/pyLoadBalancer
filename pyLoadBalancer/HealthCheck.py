8894d311efff4fe82d78dc86cf9e40b2daacf249
Yannick Martin <janiick@gmail.com>
2016-11-29 15:28:57 +0100

    gub fix

--- pyLoadBalancer/HealthCheck.py
+++ pyLoadBalancer/HealthCheck.py
@@ -81,7 +81,7 @@ class HealthCheck:
         time.sleep(2*self.CONSTANTS['SOCKET_TIMEOUT']/1000.)
 
         for workerid in self.workers:
-            self.workers[workerid]['workerstate'] = 0
+            self.workers[workerid]['workerstate'] = -1
         failed = 0
 
 
@@ -149,7 +149,7 @@ class HealthCheck:
 
         # If the worker has not done anything since 2 minutes, tell LB it is idle
         for workerid in self.workers:
-            if ((time.time() - self.workers[workerid]['lasttasktime']) > 10) and (self.workers[workerid]['workerstate'] == 0):
+            if ((time.time() - self.workers[workerid]['lasttasktime']) > 1) and (self.workers[workerid]['workerstate'] == -1):
                 cprint('HC - %s SEEMS DOWN (%ss and state=%s)'%(workerid,(time.time() - self.workers[workerid]['lasttasktime']),self.workers[workerid]['workerstate']), 'OKBLUE')
                 self.downWorker(workerid)
 
